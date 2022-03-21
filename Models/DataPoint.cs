40a05398e4c2563e7e8b6ecb98c2182a6e254cd3
SamratGhale <Samrat.biz19@gmail.com>
2022-03-22 01:59:56 +0545

    added charts, removed tables from deatils page, added full name

---
+++ Models/DataPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+namespace berkeley_college.Models
+{
+    public class DataPoint
+    {
+        //Explicitly setting the name to be used while serializing to JSON.
+        public double X { get; set; }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        public string Y { get; set;  }
+    }
+}
